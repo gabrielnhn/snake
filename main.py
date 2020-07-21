d3eb1931c571e80418f272e0606f3aaf0f6c98ef
Gabriel Nascarella <ielnascarella@hotmail.com>
2020-07-21 18:50:36 -0300

    Fixes occasional 'curses.addstr error' by checking the size of the terminal beforehand.

--- main.py
+++ main.py
@@ -10,49 +10,66 @@ from apple import Apple
 from main_functions import (new_key, column_center, set_board, print_board,
                             new_position, game_over)
 
-# setup:
+
+def game(scr):
+
+    # setup:
+    score = 0
+    snake = Snake()
+    apple = Apple()
+    key = KEY_RIGHT
+
+    # game loop:
+    while True:    
+        # print screen
+        board = Board()
+        set_board(board, snake, apple)
+        print_board(scr, board, score)
+        sleep(DELAY_TIME)
+
+        # process new input
+        key = new_key(scr, key)
+        if chr(key) == EXIT_KEY:
+            break
+
+        # process new game state:
+        # get next position
+        i, j = new_position(snake, key)
+
+        new_position_char = board.get_coord(i, j)
+
+        # check new position
+        if new_position_char == APPLE_CHAR:
+            # eat apple:
+            snake.increase_to(i, j)
+            score += 1
+        elif new_position_char == SNAKE_CHAR:
+            # bumped into itself:
+            game_over(scr)
+            break
+        else:
+            # empty space:
+            snake.move_to(i, j)
+
+        # check if apple was eaten:
+        apple.check(board)
+
+    
+    # return total score:
+    return score
+    
+
+
+# main program:
+
 scr = init_curses()
-score = 0
-snake = Snake()
-apple = Apple()
-key = KEY_RIGHT
+height, width = scr.getmaxyx()
 
-# game loop:
-while True:    
-    # print screen
-    board = Board()
-    set_board(board, snake, apple)
-    print_board(scr, board, score)
-    sleep(DELAY_TIME)
+if (height < LINES + 3) or (width < COLUMNS + 1):
+    terminate_curses(scr)
+    print("Terminal is too small to play the game")
 
-    # process new input
-    key = new_key(scr, key)
-    if chr(key) == EXIT_KEY:
-        break
-
-    # process new game state:
-    # get next position
-    i, j = new_position(snake, key)
-
-    new_position_char = board.get_coord(i, j)
-
-    # check new position
-    if new_position_char == APPLE_CHAR:
-        # eat apple:
-        snake.increase_to(i, j)
-        score += 1
-    elif new_position_char == SNAKE_CHAR:
-        # bumped into itself:
-        game_over(scr)
-        break
-    else:
-        # empty space:
-        snake.move_to(i, j)
-
-    # check if apple was eaten:
-    apple.check(board)
-
-terminate_curses(scr)
-
-# print total score:
-print("Score: {}".format(score))
+else:
+    score = game(scr)
+    terminate_curses(scr)
+    print("Score: {}".format(score))
