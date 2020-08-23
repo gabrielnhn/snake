2f508d3b38baa54063343e2c6da4cf6b886af1b9
Gabriel Nascarella <ielnascarella@hotmail.com>
2020-08-23 14:03:30 -0300

    Add comments for each setting

--- configs.py
+++ configs.py
@@ -1,19 +1,28 @@
+# Change the game settings here!
+# Available colors:
 from curses import (COLOR_BLACK, COLOR_BLUE, COLOR_CYAN, COLOR_GREEN,
                     COLOR_MAGENTA, COLOR_RED, COLOR_WHITE, COLOR_YELLOW)
 
-LINES = 13
-COLUMNS = 13
 
+# Board size:
+LINES = 12
+COLUMNS = 12
+
+# Snake configs:
 SNAKE_CHAR = "O"
 SNAKE_COLOR = COLOR_GREEN
 INITIAL_SIZE = 2
 
+# Apple appearance:
 APPLE_CHAR = "*"
 APPLE_COLOR = COLOR_RED
 
+# Empty coordinate appearance:
 EMPTY_CHAR = "'"
 EMPTY_COLOR = COLOR_WHITE
 
-REFRESH_TIME = 0.1 # seconds
-
+# Text appearance:
 TEXT_COLOR = COLOR_YELLOW
+
+# Game pacing:
+REFRESH_TIME = 0.11 # seconds
